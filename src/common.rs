ddce7ceaf86a4c8f91b454e838127ad7afb0ddfd
敷衍 <fuyan@fuyandeMac-mini.local>
2025-09-09 23:30:24 +0800

    api地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://nice.osapp.vip".to_owned()
+    "http://nice.osapp.vip:21114".to_owned()
 }
 
 #[inline]
